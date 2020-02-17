4a5618b095b6b9bbddaf8c4f8aaf4b8579828636
Denys Krytskyi <didistars328@gmail.com>
2020-02-17 20:49:31 +0200

    gitignore update

--- project/prod/var.tf
+++ project/prod/var.tf
@@ -2,6 +2,7 @@ variable "AWS_REGION" { default = "eu-central-1" }
 variable "PORTS" {
   type = map(list(string))
   default = {
-    22 = ["0.0.0.0/0"]
+    "22" = ["0.0.0.0/0"]
+    "80" = ["0.0.0.0/0"]
   }
 }
