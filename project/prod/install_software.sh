23ef46c04d542ce9a287fe32c0b1bad3f7085604
Denys Krytskyi <didistars328@gmail.com>
2020-02-18 14:47:20 +0200

    test hooks 5

--- project/prod/install_software.sh
+++ project/prod/install_software.sh
@@ -4,5 +4,5 @@ amazon-linux-extras install docker
 service docker start
 usermod -a -G docker ec2-user
 amazon-linux-extras install nginx1.12
-echo "This AMI was generated at $(date +%m-%d-%Y)" > /root/amigen_file.txt
+echo "This AMI was generated at $(date +%m-%d-%Y)" > /root/AMI_gen_file.txt
 
