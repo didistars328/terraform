cf4e941b40d9fe52aada55e6fa588594a4535ea5
Denys Krytskyi <didistars328@gmail.com>
2020-02-19 09:27:26 +0200

    Add test sh script

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+folder=project/prod
+diff="$(git diff HEAD^ --name-only --diff-filter=ACMR "$folder")"
+echo $diff
+if [ ! -z "$diff" ]
+    then
+    echo "It works"
+    else
+      echo "Doesn't work"
+    fi
