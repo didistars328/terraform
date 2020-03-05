171157fccdf1620170fc9dee707e2236622dd0b6
Denys Krytskyi <didistars328@gmail.com>
2020-03-05 15:19:33 +0200

    Module demo folder

--- ecs-modules/ecs.tf
+++ ecs-modules/ecs.tf
@@ -1,35 +1,54 @@
-# 1. Add ECS cluster
-resource "aws_ecs_cluster" "cluster-ecs" {
-  name = "cluster-ecs"
-}
-# 2. Add config to ECS
-resource "aws_launch_configuration" "ecs-launchconfig" {
-  name_prefix          = "ecs-launchconfig"
-  image_id             = var.ECS_AMIS[var.AWS_REGION]
-  instance_type        = var.ECS_INSTANCE_TYPE
-  key_name             = aws_key_pair.mykeypair.key_name
-  iam_instance_profile = aws_iam_instance_profile.ecs-ec2-role.id
-  security_groups      = [aws_security_group.ecs-security-group.id]
-  user_data            = <<EOF
-     #!/bin/bash
-     echo 'ECS_CLUSTER=cluster-ecs' > /etc/ecs/ecs.config
-     start ecs
-     EOF
-  lifecycle {
-    create_before_destroy = true
-  }
+data "aws_caller_identity" "current" {
 }
 
-# 3. Add autoscaling group
-resource "aws_autoscaling_group" "ecs-autoscaling" {
-  name                 = "ecs-autoscaling"
-  launch_configuration = aws_launch_configuration.ecs-launchconfig.name
-  vpc_zone_identifier  = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
-  min_size             = 1
-  max_size             = 1
-  tag {
-    key                 = "Name"
-    value               = "ecs-ec2-container"
-    propagate_at_launch = true
-  }
+module "my-ecs" {
+  source         = "github.com/in4it/terraform-modules//modules/ecs-cluster?ref=terraform-0.12"
+  VPC_ID         = module.vpc.vpc_id
+  CLUSTER_NAME   = "my-ecs"
+  INSTANCE_TYPE  = "t2.micro"
+  SSH_KEY_NAME   = aws_key_pair.mykeypair.key_name
+  VPC_SUBNETS    = join(",", module.vpc.public_subnets)
+  ENABLE_SSH     = true
+  SSH_SG         = aws_security_group.allow-ssh.id
+  LOG_GROUP      = "my-log-group"
+  AWS_ACCOUNT_ID = data.aws_caller_identity.current.account_id
+  AWS_REGION     = var.AWS_REGION
 }
+
+module "my-service" {
+  source              = "github.com/in4it/terraform-modules//modules/ecs-service?ref=terraform-0.12"
+  VPC_ID              = module.vpc.vpc_id
+  APPLICATION_NAME    = "my-service"
+  APPLICATION_PORT    = "80"
+  APPLICATION_VERSION = "latest"
+  CLUSTER_ARN         = module.my-ecs.cluster_arn
+  SERVICE_ROLE_ARN    = module.my-ecs.service_role_arn
+  AWS_REGION          = var.AWS_REGION
+  HEALTHCHECK_MATCHER = "200"
+  CPU_RESERVATION     = "256"
+  MEMORY_RESERVATION  = "128"
+  LOG_GROUP           = "my-log-group"
+  DESIRED_COUNT       = 2
+  ALB_ARN             = module.my-alb.alb_arn
+}
+
+module "my-alb" {
+  source             = "github.com/in4it/terraform-modules//modules/alb?ref=terraform-0.12"
+  VPC_ID             = module.vpc.vpc_id
+  ALB_NAME           = "my-alb"
+  VPC_SUBNETS        = join(",", module.vpc.public_subnets)
+  DEFAULT_TARGET_ARN = module.my-service.target_group_arn
+  DOMAIN             = "*.didistars328.tk"
+  INTERNAL           = false
+  ECS_SG             = module.my-ecs.cluster_sg
+}
+
+module "my-alb-rule" {
+  source           = "github.com/in4it/terraform-modules//modules/alb-rule?ref=terraform-0.12"
+  LISTENER_ARN     = module.my-alb.http_listener_arn
+  PRIORITY         = 100
+  TARGET_GROUP_ARN = module.my-service.target_group_arn
+  CONDITION_FIELD  = "host-header"
+  CONDITION_VALUES = ["subdomain.didistars328.tk"]
+}
+
---
+++ ecs-modules/vars.tf
@@ -0,0 +1,48 @@
+variable "AWS_REGION" {
+  default = "eu-central-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "ECS_AMIS" {
+  type = map(string)
+  default = {
+    us-east-1    = "ami-062f7200baf2fa504"
+    us-west-2    = "ami-04590e7389a6e577c"
+    eu-central-1 = "ami-0bfdae54e0eda93f2" # ECS AMI exect ami optimized for ECS only
+  }
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
+}
+
+variable "ECS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
+
+variable "MYAPP_SERVICE_ENABLE" {
+  default = "0"
+}
+
+variable "REPO_NAME" {
+  default = "0"
+}
+
+variable "MYAPP_VERSION" {
+  default = "0"
+}
+
+variable "REPOSITORY" {
+  default = "0"
+}
